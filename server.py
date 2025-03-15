00fbad230a7eccaab73810478df2ac8da49f5f7c
udf <tabhooked@gmail.com>
2025-03-15 17:43:43 +0200

    only set max-age on incomplete responses

--- server.py
+++ server.py
@@ -78,7 +78,7 @@ async def api_get_root(req: web.Request):
   return web.Response(
     body=template.get_html(),
     content_type='text/html',
-    headers={'Cache-Control': 'max-age=600'} if metadata.is_complete else None
+    headers={'Cache-Control': 'public' if metadata.is_complete else 'max-age=5' }
   )
 
 
