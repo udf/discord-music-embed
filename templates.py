e286b8f54fdbdda1b0a99743263feada4613c54a
udf <tabhooked@gmail.com>
2025-03-17 19:01:09 +0200

    escape values in html
    lol

--- templates.py
+++ templates.py
@@ -1,6 +1,22 @@
+import string
+import html
+from typing import override
 from config import PAGE_TITLE, SITE_NAME, THEME_COLOR
 from metadata_service import AudioMetadata
 from metadata_tags import Tags
+
+
+class HTMLFormatter(string.Formatter):
+  @override
+  def format_field(self, value, format_spec: str):
+    is_attr = False
+    if format_spec == 'attr':
+      format_spec = ''
+      is_attr = True
+    return html.escape(
+      format(value, format_spec),
+      quote=is_attr
+    )
 
 
 def get_album_info(tags: Tags):
@@ -21,43 +37,59 @@ def get_html(
   song_info = f'{tags.artist} - {tags.title}' if tags.artist else tags.title
   album_info = get_album_info(tags)
   site_name = f'{SITE_NAME} | {album_info}' if album_info else SITE_NAME
+  cover_width = meta.cover_width
+  cover_height = meta.cover_height
 
-  return f'''
+  return FORMATTER.format('''
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta property="og:site_name" content="{site_name}">
-  <meta property="og:title" content="{song_info}">
-  <meta property="og:description" content="{song_info}">
-  <meta property="og:image" content="{cover_url}">
-  <meta name="theme-color" content="{THEME_COLOR}" />
+  <meta property="og:site_name" content="{site_name:attr}">
+  <meta property="og:title" content="{song_info:attr}">
+  <meta property="og:description" content="{song_info:attr}">
+  <meta property="og:image" content="{cover_url:attr}">
+  <meta name="theme-color" content="{THEME_COLOR:attr}" />
   <meta property="og:type" content="video.other">
 
-  <meta property="og:video" content="{content_url}">
-  <meta property="og:video:secure_url" content="{content_url}">
+  <meta property="og:video" content="{content_url:attr}">
+  <meta property="og:video:secure_url" content="{content_url:attr}">
   <meta property="og:video:type" content="video/mp4">
-  <meta property="og:video:width" content="{meta.cover_width}">
-  <meta property="og:video:height" content="{meta.cover_height}">
+  <meta property="og:video:width" content="{cover_width:attr}">
+  <meta property="og:video:height" content="{cover_height:attr}">
   <title>{song_info} | {PAGE_TITLE}</title>
 </head>
 <body>
   <h1>{PAGE_TITLE}</h1>
   <h2>{song_info}</h2>
   <h3>{album_info}</h3>
-  <img src="{cover_url}" width="300" height="300" style="object-fit: contain;" />
+  <img src="{cover_url:attr}" width="300" height="300" style="object-fit: contain;" />
   <br/>
-  <audio controls src="{content_url}"></audio>
+  <audio controls src="{content_url:attr}"></audio>
   <br/>
   <span>
-    <a href="{content_url}">Link</a>
+    <a href="{content_url:attr}">Link</a>
     <span>|</span>
-    <a href="{content_url}" download>Download</a>
+    <a href="{content_url:attr}" download>Download</a>
   </span>
   <hr/>
   <p>Generated at: {gmt_now}</p>
 </body>
 </html>
-'''
+''',
+  THEME_COLOR=THEME_COLOR,
+  PAGE_TITLE=PAGE_TITLE,
+  site_name=site_name,
+  song_info=song_info,
+  cover_url=cover_url,
+  content_url=content_url,
+  cover_width=cover_width,
+  cover_height=cover_height,
+  album_info=album_info,
+  gmt_now=gmt_now,
+)
+
+
+FORMATTER = HTMLFormatter()
