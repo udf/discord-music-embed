af98bd3e2ff387700cd8aaf2c11393cd2eae7b0c
udf <tabhooked@gmail.com>
2025-03-18 22:36:32 +0200

    clear file indexer cache on worker processes

--- metadata_service.py
+++ metadata_service.py
@@ -17,6 +17,7 @@ import db
 from db import CachedAudioMetadata
 from config import COVER_DIR, DEFAULT_COVER_PATH, MUSIC_DIR, METADATA_WORKERS
 from metadata_tags import Tags, read_audio_tags
+import file_indexer
 
 
 ACCEPTED_FILE_EXTS = {'.flac', '.mp3'}
@@ -29,6 +30,7 @@ def _mp_init():
   global logger
   current_proc = multiprocessing.current_process()
   logger = logging.getLogger(f'meta:{current_proc.name}')
+  file_indexer.discovered_files = set()
 
 
 @dataclass
